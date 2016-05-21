1af361791fe930a76de0c389998c9b121e91c160
Sig <sigbikes@gmail.com>
2016-05-21 10:44:32 -0500

    Chapter 1 Exercises - FINAL UPDATE
    2016/05/21 10:43AM - JTS - Seriously the final update to chapter 1.

--- tango_django_project/rango/views.py
+++ tango_django_project/rango/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    response = HttpResponse("Rango says hey there partner!")
+    response.write("<br>")
+    response.write("<a href='/rango/about'>About</a>")
+    return response
+
+
+
+def about(request):
+    response = HttpResponse('Rango says "Here is an about page".')
+    response.write("<br><br><a href='/rango/'>Home</a>")
+
+    return response
