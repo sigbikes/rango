ed0ce547178c173bd5c71a201fc36bd0c2294239
Sig <sigbikes@gmail.com>
2016-06-25 14:58:37 -0500

    2016/06/24 - JTS - Updates views and index to utilize database and templates

--- tango_django_project/rango/admin.py
+++ tango_django_project/rango/admin.py
@@ -7,5 +7,8 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ['title', 'url', 'category', 'views']
 
-admin.site.register(Category)
+class CatAdmin(admin.ModelAdmin):
+    list_display = ['name', 'views', 'likes']    
+
+admin.site.register(Category, CatAdmin)
 admin.site.register(Page, PageAdmin)
--- tango_django_project/rango/views.py
+++ tango_django_project/rango/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, HttpResponse
+# Import Category model for index page
+from rango.models import Category
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{boldmessage}} in the template!
-    context_dict = {'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descinding order.
+    # Retriever the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
-
-
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 
