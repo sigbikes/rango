df8ec2dab0cda641c79c6a232c9f9626ab247914
Sig <sigbikes@gmail.com>
2016-06-28 17:45:50 -0500

    2016/06/28 - JTS - Added slug field to model and updated DB

--- tango_django_project/rango/admin.py
+++ tango_django_project/rango/admin.py
@@ -8,7 +8,8 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ['title', 'url', 'category', 'views']
 
 class CatAdmin(admin.ModelAdmin):
-    list_display = ['name', 'views', 'likes']    
+    prepopulated_fields = {'slug':('name',)}
+    list_display = ['name', 'views', 'likes', 'slug']
 
 admin.site.register(Category, CatAdmin)
 admin.site.register(Page, PageAdmin)
--- tango_django_project/rango/models.py
+++ tango_django_project/rango/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.template.defaultfilters import slugify
 # Create your models here.
 
 
@@ -9,6 +9,11 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'categories'
